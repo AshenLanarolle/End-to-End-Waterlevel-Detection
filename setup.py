c9d597d15335f9b2cd7142fbd5798d80abf471c3
AshenLanarolle <ashenkavishka097@gmail.com>
2024-06-05 11:44:14 +0530

    requirments

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import  find_packages, setup
+
+setup(
+    name = "LiquidLevelDetection",
+    version = '0.0.0',
+    author = 'AshenLanarolle',
+    author_email= "ashenkavishkao97@gmail.com",
+    packages= find_packages(),
+    install_requires =[]
+
+)
