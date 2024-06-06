265682a39352d671d312f7d45e2f6ea0021d0484
AshenLanarolle <ashenkavishkao97@gmail.com>
2024-06-06 16:38:26 +0530

    Data Ingestion

---
+++ LiquidLevelDetection/constant/training_pipeline/__init__.py
@@ -0,0 +1,14 @@
+import os
+
+ARTIFACTS_DIR: str = "artifacts"
+
+"""
+Data ingestion related constant start with DATA_INGESTION VAR NAME
+
+"""
+
+DATA_INGESTION_DIR_NAME: str = "data_ingestion"
+
+DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
+
+DATA_DOWNLOARD_URL: str = "https://github.com/AshenLanarolle/Datasets/raw/main/Water%20level%20Detection.zip"
